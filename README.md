8cdbc9e447b6f4a32bb3e2f4366128d4e2c53bda
Ray Raven <rayzafir@aol.com>
2018-09-22 16:45:14 +0000

    automatically backed up by learn

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## Objectives
+ longPlaneteerCalls## Objectives
 
 You're going to get familiar with iterating through arrays in Javascript. 
 
--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 function dwarfRollCall(dwarves) {
+<<<<<<< HEAD
   var array=[];
  for (var i=0; i<dwarves.length ; i++) {
  array.push(`${i+1}. ${dwarves[i]} `);
   }
  return array.join('');
  }
+=======
+ for (var i=0; i<dwarves.length ; i++) {
+  return ` ${i+1}. ${dwarves[i]}`
+ }
+}
+
+>>>>>>> 943164d3019d0361ae440b13e776a1d0b91738b7
 function summonCaptainPlanet(planeteerCalls){
 }
 
